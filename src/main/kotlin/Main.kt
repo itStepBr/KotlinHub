002f13a47295a32df5e6a3cfbec481198c227720
ITs <brestitstep1@gmail.com>
2024-10-31 11:42:15 +0300

    add new function:buttonSubmit_edit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,7 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-
-    println("Hello, " + name + "!")
-    idea()
     buttonSubmit()
-
-}
-fun idea(){
-    println("new data")
-
 }
 fun buttonSubmit(){
     println("Ваши данные успешно отправлены")
